2e6b220ede506085cfc54b822438d71ac2e2d4e2
Aaron Blum <blumaa@gmail.com>
2019-08-03 04:38:46 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,38 @@
+require 'pry'
 # Write your code here!
 
+game_hash = {
+  home: {
+    team_name: "",
+# colors key points to an array of strings that are team's colors
+    colors: [], 
+    players: [{
+      number: [],
+      shoe: [],
+      points: [],
+      rebounds: [],
+      assists: [],
+      steals: [],
+      blocks: [],
+      slam_dunks: []
+      }]
+    },
+  away: {
+    team_name: "",
+# colors key points to an array of strings that are team's colors
+    colors: [], 
+    players: [{
+      number: [],
+      shoe: [],
+      points: [],
+      rebounds: [],
+      assists: [],
+      steals: [],
+      blocks: [],
+      slam_dunks: []
+      }]
+    }
+}
 
 
 
@@ -9,3 +42,4 @@
 
 
 
+binding.pry
